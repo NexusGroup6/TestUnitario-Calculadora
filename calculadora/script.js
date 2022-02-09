c596bb7a0b13bf977cafc2b892f583de6de4a937
Vitor Benedito <vitor.bsdc@gmail.com>
2022-02-09 14:55:27 -0300

    Test Unitário - Function soma

--- calculadora/script.js
+++ calculadora/script.js
@@ -1,7 +1,7 @@
 // function calc1() {/*Função 1 adição*/
 //     var valor1 = document.querySelector('#input1').value;/*Armazenar valor de input 1*/
 //     var valor2 = document.querySelector('#input2').value;/*Armazenar valor de input 2*/
-//     var result1 = parseFloat(valor1) + parseFloat(valor2);/*Função em si*/
+//     var result1 = parseInt(valor1) + parseInt(valor2);/*Função em si*/
 //     document.querySelector('.result').innerHTML = 'Resultado: ' + result1;/*Imprimir resultado para user*/
 // }
 
